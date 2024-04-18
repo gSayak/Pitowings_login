afd06afe35bd23374da7e436e32f482390474591
Pranshu321 <pranshujain0111@gmail.com>
2024-04-18 22:54:02 +0530

    Update route path for forget password page in App.tsx and implement email validation in ForgetPassword.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/forgot" element={<ForgetPassword />} />
+          <Route path="/forget" element={<ForgetPassword />} />
         </Routes>
       </Router>
     </div>

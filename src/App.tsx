491158a1f650c4fc34fdffbdd655b59f29a05cbb
gSayak <gsayak22@gmail.com>
2024-04-18 14:40:45 +0530

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Signup from './components/Signup';
 
 function App() {
   return (
-    <div className='text-3xl text-red-600'>
-      Hello
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }

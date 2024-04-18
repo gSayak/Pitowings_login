998b746e647ec83842f32bc0c81beb019fb7dab3
Pranshu321 <pranshujain0111@gmail.com>
2024-04-18 14:20:21 +0530

    Added typescript template for base

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='text-3xl text-red-600'>
+      Hello
+    </div>
+  );
+}
+
+export default App;

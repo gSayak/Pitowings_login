a12a3bf47529316ba399d36e0ec75bc8e1e62662
saifriaz001 <89684150+saifriaz001@users.noreply.github.com>
2024-04-19 18:06:00 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import Login from './components/Login';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Signup from './components/Signup';
+import NewUser from './components/newUser/NewUser';
 import ForgetPassword from './components/ForgetPassword/ForgetPassword';
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/signup" element={<NewUser/>} />
           <Route path="/forgot" element={<ForgetPassword />} />
         </Routes>
       </Router>
